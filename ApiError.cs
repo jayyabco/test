aa5b94504e36f23bd11ec12911aedd1834eb1377
jayyabco <devloperx@hotmail.com>
2025-11-13 20:08:16 +0100

    Add files via upload

---
+++ ApiError.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApplication1
+{
+    public class ApiError
+    {
+        public ApiError()
+        {
+        }
+
+        public string Message { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+
+        [JsonIgnore]
+        public string StackTrace { get; set; } = string.Empty;
+
+        [JsonPropertyName("stackTrace")]
+        public string StackTracePublic => string.Empty;
+    }
+}
---
+++ ApiLoggingItem.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication1
+{
+    public class ApiLoggingItem
+    {
+        public ApiLoggingItem()
+        {
+        }
+
+        public ApiLoggingItem(string msg, string pStatus, string pTrace)
+        {
+            Message = msg;
+            Status = pStatus;
+            Trace = pTrace;
+        }
+
+        public string Message { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string Trace { get; set; } = string.Empty;
+    }
+}
